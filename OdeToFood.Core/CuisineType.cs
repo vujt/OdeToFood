ad32f419407025e96db5f3d5527c97004e4d881c
Johnny Vu <vujt@hotmail.com>
2020-06-25 12:46:51 -0700

    Add project files.

---
+++ OdeToFood.Core/CuisineType.cs
@@ -0,0 +1,10 @@
+﻿namespace OdeToFood.Core
+{
+    public enum CuisineType
+    {
+        none,
+        Mexican,
+        Italian,
+        Indian
+    }
+}
---
+++ OdeToFood.Core/Restaurant.cs
@@ -0,0 +1,12 @@
+﻿namespace OdeToFood.Core
+{
+
+    public class Restaurant
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+
+        public CuisineType Cuisine { get; set; }
+    }
+}
