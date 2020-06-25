7e1675c16d74bfef501f836e3553633793a755f1
Johnny Vu <vujt@hotmail.com>
2020-06-25 14:36:12 -0700

    add error handling

---
+++ OdeToFood/Pages/Restaurants/Detail.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using OdeToFood.Core;
+using OdeToFood.Data;
+
+namespace OdeToFood.Pages.Restaurants
+{
+    public class DetailModel : PageModel
+    {
+        private readonly IRestaurantData restaurant;
+
+        public Restaurant Restaurant { get; set; }
+
+        public DetailModel(IRestaurantData restaurant)
+        {
+            this.restaurant = restaurant;
+        }
+
+        public IActionResult OnGet(int restaurantId)
+        {
+            Restaurant = restaurant.GetById(restaurantId);
+
+            if (Restaurant == null)
+            {
+                return RedirectToPage("./NotFound");
+            }
+
+            return Page();
+        }
+
+        
+    }
+}
